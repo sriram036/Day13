ae67be82b11c5be24947c41292ebe7498cce9305
Ravi@036# <admin@admins-MacBook-Pro.local>
2023-05-01 19:39:32 +0530

    added: find max float

--- src/com/bridgelabz/GenericsMain.java
+++ src/com/bridgelabz/GenericsMain.java
@@ -2,7 +2,7 @@ package com.bridgelabz;
 
 public class GenericsMain {
 
-    static <T extends Comparable<T>>T findMaxInteger(T x, T y, T z){
+    static <T extends Comparable<T>>T findMaxFloat(T x, T y, T z){
         T max = x;
         if (y.compareTo(max) > 0){
             max = y;
@@ -15,6 +15,6 @@ public class GenericsMain {
 
     public static void main(String[] args) {
 
-        System.out.println("Max number of given  integers is " + findMaxInteger(222,18,122));
+        System.out.println("Max number of given  integers is " + findMaxFloat(222.1F,18.23F,122.32F));
     }
 }
